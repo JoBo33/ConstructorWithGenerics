98190da5b872ad367392b9d9b582015b1faa127f
jb83292 <jb83292@LAP122-0529>
2022-07-15 15:14:15 +0200

    Projektdateien hinzufügen.

---
+++ ConstructorPractice/Calculate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPractice
+{
+    public class Calculate<T>
+    {
+        double a;
+        double b;
+
+        public Calculate()
+        {
+        }
+        public Calculate(T a) :this()
+        {
+            this.a = Convert.ToDouble(a);
+        }
+        public Calculate(T a, T b) : this(a)
+        {
+            this.b = Convert.ToDouble(b);
+        }
+
+
+        public double GetA() => a;
+        
+        public void SetA(T a)
+        {
+            this.a = Convert.ToDouble(a);
+        }
+
+        public double GetB() => b;
+        
+        public void SetB(T b) 
+        {
+            this.b = Convert.ToDouble(b);    
+        }
+
+        public double Add() => a + b;
+        public double Sub() => a - b;
+        public double Multiply() => a * b;
+        public double Divide() => a / b;
+
+    }
+}
---
+++ ConstructorPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate<int> calc1 = new Calculate<int>(8, 4);
+            Console.WriteLine(calc1.Add());
+            Console.WriteLine(calc1.Sub());
+            Console.WriteLine(calc1.Multiply());
+            Console.WriteLine(calc1.Divide());
+
+            Console.WriteLine("\n");
+
+            Calculate<double> calc2 = new Calculate<double>(15.6, 5.2);
+            Console.WriteLine(calc2.Add());
+            Console.WriteLine(calc2.Sub());
+            Console.WriteLine(calc2.Multiply());
+            Console.WriteLine(calc2.Divide());
+
+            Console.WriteLine("\n");
+
+            Calculate<string> calc3 = new Calculate<string>("18", "6");
+            Console.WriteLine(calc3.Add());
+            Console.WriteLine(calc3.Sub());
+            Console.WriteLine(calc3.Multiply());
+            Console.WriteLine(calc3.Divide());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
